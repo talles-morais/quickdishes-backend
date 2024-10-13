7f81fb43527a7c810cec007c43f1b369c71d412a
Talles <talves2503@gmail.com>
2024-10-13 19:40:32 -0300

    feat: migration setup

--- database/db.go
+++ database/db.go
@@ -3,15 +3,34 @@ package database
 import (
 	"log"
 
+	"github.com/talles-morais/quick-dishes/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func ConnectDB()  {
-	dsn := "host=localhost user=root password=root dbname=dcs-postgres port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{}); 
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func ConnectDB() {
+	dsn := "host=localhost user=root password=root dbname=postgres port=5432 sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(dsn))
 	if err != nil {
 		log.Panic("error connecting to database")
 	}
-	db.AutoMigrate()
+
+	if err := DB.AutoMigrate(&models.Restaurant{}); err != nil {
+		log.Panic("migration error, restaurant model")
+	}
+	if err := DB.AutoMigrate(&models.Client{}); err != nil {
+		log.Panic("migration error, client model")
+	}
+	if err := DB.AutoMigrate(&models.Product{}); err != nil {
+		log.Panic("migration error, product model")
+	}
+	if err := DB.AutoMigrate(&models.Order{}); err != nil {
+		log.Panic("migration error, order model")
+	}
+
 }
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/talles-morais/quick-dishes/routes"
+import (
+	"github.com/talles-morais/quick-dishes/database"
+	"github.com/talles-morais/quick-dishes/routes"
+)
 
 func main() {
+	database.ConnectDB()
 	routes.Router()
 }
