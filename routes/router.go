df8ce984632357985fe976c839c5ee30e938865a
Talles <talves2503@gmail.com>
2024-10-25 02:01:59 -0300

    feat: setup CORS policy

--- routes/router.go
+++ routes/router.go
@@ -3,12 +3,16 @@ package routes
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/talles-morais/quick-dishes/controllers"
 )
 
 func Router() {
 	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	router.Use(cors.New(config))
 	router.GET("/", func(ctx *gin.Context) { ctx.JSON(http.StatusOK, gin.H{"hello": "world"}) })
 	router.POST("/login", controllers.LoginRestaurant)
 	router.POST("/signup", controllers.CreateRestaurant)
